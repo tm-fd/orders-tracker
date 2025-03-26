e66782910cbf7b4f58b95c92c56388077614c639
tm-fd <mr.tm84@gmail.com>
2025-03-26 12:21:18 +0100

    remove login with google

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -153,7 +153,7 @@ export default function SignIn() {
             )}
           </AnimatePresence>
           <div className=" flex flex-col gap-4">
-          <button className="inline-flex items-center justify-center gap-3 py-3 text-sm font-normal text-gray-700 transition-colors bg-gray-100 rounded-lg px-7 hover:bg-gray-200 hover:text-gray-800 dark:bg-white/5 dark:text-white/90 dark:hover:bg-white/10">
+          {/* <button className="inline-flex items-center justify-center gap-3 py-3 text-sm font-normal text-gray-700 transition-colors bg-gray-100 rounded-lg px-7 hover:bg-gray-200 hover:text-gray-800 dark:bg-white/5 dark:text-white/90 dark:hover:bg-white/10">
                 <svg
                   width="20"
                   height="20"
@@ -203,7 +203,7 @@ export default function SignIn() {
                   Or
                 </span>
               </div>
-            </div>
+            </div> */}
           <Form
             className="flex flex-col gap-4 max-[600px]:w-full"
             validationErrors={error}
--- src/app/loading.tsx
+++
@@ -1,7 +0,0 @@
-import {
-    Spinner,
-  } from "@heroui/react";
-
-export default function Loading({style_inline}) {
-    return <div className="loader" style={style_inline}></div> 
-  }
